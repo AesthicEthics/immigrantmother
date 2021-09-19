afbadf26eb92f1a0d9d138d0bf7bd4a1478d1129
Aryan Thakur <70349501+AesthicEthics@users.noreply.github.com>
2021-09-19 04:24:27 -0400

    Add files via upload

---
+++ website/client/src/home.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Navbar, Nav, Container} from "react-bootstrap";
+import "./App.css";
+
+function Home() {
+
+
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container className="navMargin">
+                <Navbar.Brand>Immigrant Parent</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About Us</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    {/* <Nav.Link eventKey={2} href="#memes">
+                    Dank memes
+                    </Nav.Link> */}
+                </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
