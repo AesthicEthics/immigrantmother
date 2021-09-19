585aae4f8f6ed21ab4abc20b55889e4e7c86fad9
Varsha Prasad <64542485+vavaviper@users.noreply.github.com>
2021-09-19 02:11:39 -0400

    finished

--- focus_checker.py
+++ focus_checker.py
@@ -4,6 +4,7 @@ import numpy as np
 import mne
 import brainflow
 import matplotlib.pyplot as plt
+from datetime import datetime
 import time
 import pyautogui 
 
@@ -21,8 +22,8 @@ MLModel.enable_ml_logger()
 
 #adding info for the board
 params = BrainFlowInputParams()
-params.board_id = -1
-board_id = -1
+params.board_id = 1
+board_id = 1
 params.serial_port = 'COM3'
 sampling_rate = BoardShim.get_sampling_rate(board_id)
 BoardShim.enable_dev_board_logger()
@@ -38,50 +39,60 @@ concentration_values = []
 concentration_values = np.array(concentration_values)
 timestamps = []
 are_they_a = []
-data = board.get_board_data()
 
+eeg_channels = [0,1]
 
-if boolean == True:
-
-
-    eeg_channels = BoardShim.get_eeg_channels(int(board_id))
-
-    #data filtering
-    for count, channel in enumerate(eeg_channels):
-        DataFilter.perform_bandstop(data[channel], sampling_rate, 60.0, 4.0, 4,
-        FilterTypes.BUTTERWORTH.value, 0)  # bandstop 58 - 62
-        DataFilter.perform_bandpass(data[channel], sampling_rate, 21.0, 20.0, 4,
-        FilterTypes.BESSEL.value, 0)  # bandpass 11 - 31
-
-
-    bands = DataFilter.get_avg_band_powers(data, eeg_channels, sampling_r.
-    ate, True)
-    feature_vector = np.concatenate((bands[0], bands[1]))
-    print(feature_vector)
-    print(data)
-
-    #concentration
-    concentration_params = BrainFlowModelParams(BrainFlowMetrics.CONCENTRATION.value, BrainFlowClassifiers.KNN.value)
-    concentration = MLModel(concentration_params)
-    concentration.prepare()
-    predict_value = concentration.predict(feature_vector)
-
-    print('Concentration: %f' % predict_value)
-    concentration.release()
-
-    timestamps.append(BoardShim.get_timestamp_channel(board_id))
-
-    if predict_value > 0.5:
-        are_they = 1.0
-    else:
-        are_they = 0.0
+while boolean == True:
+    for i in range(0,100):
+        if i == 99:
+            boolean = False
     
-    are_they_a.append(are_they)
+        time.sleep(2)
 
-    if pyautogui.press('enter'):
-        boolean = False
+        data = board.get_current_board_data(sampling_rate)
+        print(data)
+
+
+        #data filtering
+        for channel in eeg_channels:
+            DataFilter.perform_bandstop(data[channel], sampling_rate, 60.0, 4.0, 4,
+            FilterTypes.BUTTERWORTH.value, 0)  # bandstop 58 - 62
+            DataFilter.perform_bandpass(data[channel], sampling_rate, 21.0, 20.0, 4,
+            FilterTypes.BESSEL.value, 0)  # bandpass 11 - 31
+
+        bands = DataFilter.get_avg_band_powers(data, eeg_channels, sampling_rate, True)
+
+        feature_vector = np.concatenate((bands[0], bands[1]))
+        print(feature_vector)
+        print(data)
+
+        #concentration
+        concentration_params = BrainFlowModelParams(BrainFlowMetrics.CONCENTRATION.value, BrainFlowClassifiers.KNN.value)
+        concentration = MLModel(concentration_params)
+        concentration.prepare()
+        predict_value = concentration.predict(feature_vector)
+
+        print('Concentration: %f' % predict_value)
+        concentration.release()
+
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+
+        timestamps.append(current_time)
+
+        if predict_value > 0.1:
+            are_they = 1.0
+        else:
+            are_they = 0.0
+        
+        are_they_a.append(are_they)
+
+    
 
 
 concentration_values = [[timestamps], [are_they_a]]
+print(concentration_values)
+
 board.stop_stream()
 board.release_session()
