5f7e2a84d77acb57e2c4cd1584beca09690d78f0
Krish Patel <krishpatel338@gmail.com>
2021-09-19 11:30:37 -0400

    Update home.js

--- website/client/src/home.js
+++ website/client/src/home.js
@@ -15,18 +15,24 @@ function Home() {
                 <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/about">About Us</Nav.Link>
+                    <Nav.Link href="/dashboard">Dashboard</Nav.Link>
                 </Nav>
-                <Nav>
-                    <Nav.Link href="/login">Login</Nav.Link>
-                    {/* <Nav.Link eventKey={2} href="#memes">
-                    Dank memes
-                    </Nav.Link> */}
-                </Nav>
+                {/* <Nav>
+                    <Nav.Link href="/signout">Sign out</Nav.Link>
+                </Nav> */}
                 </Navbar.Collapse>
                 </Container>
             </Navbar>
-        <h1>Home Page</h1>
+            <div className="headingDiv">
+                <h1 className="heading">Homepage</h1>
+            </div>  
+            <div className="homediv">
+                <p className="homeP">We made a machine that yells at you or slaps you if you start losing focus or falling asleep while working, kind of like your immigrant mother.</p>
+                <p className="homeP">Using a Brain-Computer Interface, we collect brainwave data and run it through a focus/relaxation machine learning model to determine a person’s level of concentration and alertness while they are working or in a study session.</p> 
+                <p className="homeP">When it detects that they are losing focus, there are 2 different levels of increasing consequence that the machine will take; an insult yelled at them and a slap in the face</p>
+                <p className="homeP">The machine will simply yell at them first with an insult, bringing them back to alertness. After getting yelled at 3 times, they will receive a slap in the face.</p>
+                
+            </div>
         </div>
     );
 }
